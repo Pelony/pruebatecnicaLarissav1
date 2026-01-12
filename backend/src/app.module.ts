885b3047e780c0c47ef8503292735c2f4bcd4acc
PelonyDev <61895704+Pelony@users.noreply.github.com>
2026-01-12 16:34:22 -0600

    chore: initial fullstack setup with NestJS, Nuxt and Docker

---
+++ backend/src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT || 5432),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true',
+      logging: process.env.TYPEORM_LOGGING === 'true',
+    }),
+  ],
+})
+export class AppModule {}
